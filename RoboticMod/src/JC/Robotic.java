45ed53231f5b156eae9a64d672fae5e39a702408
Zurrox <Zurrox@live.com>
2013-11-04 17:11:14 +0000

    Initial Mod Commit!

---
+++ RoboticMod/src/JC/Robotic.java
@@ -0,0 +1,11 @@
+package JC;
+
+/**
+ * MinecraftDev
+ * JC.Robotic
+ *
+ * @author Zurrox
+ * @license Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License (http://creativecommons.org/licenses/by-nc-nd/3.0/)
+ */
+public class Robotic {
+}
